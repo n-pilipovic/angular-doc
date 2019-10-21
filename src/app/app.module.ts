840ea0b16efd92326810767e2feebbc173657e02
Alvar Laid <alvar.laid1@t-mobile.com>
2019-10-21 07:48:06 +0300

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpAndRoutingComponent } from './http_and_routing/http-and-routing.component';
 
 /* This is the root module of our application. Every other module that we create
 is a feature module, which in turn divide into different categories, which you can
@@ -12,11 +12,11 @@ find here: https://angular.io/guide/module-types
   // this is where we declare the components, pipes and directives that we want to use within this module
   declarations: [
     AppComponent,
+    HttpAndRoutingComponent,
   ],
   /* here we import other modules and this means we can use whatever those moduels have exported in their exports array */
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
   ],
   /* This is where we declare all of the dependencies we want to inject into our components. If you use Angular CLI to
